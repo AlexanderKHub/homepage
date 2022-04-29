ba081d27b58ab3c1b90037d287005c28463ff2c9
Alexander K <alexander.k.2000@web.de>
2022-04-30 00:32:29 +0200

    added slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//import { setTimeout } from 'timers';
+//import { setInterval } from 'timers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'alexanderkeil';
+
+export class AppComponent implements OnInit {
+  images = [
+    'laptop.webp',
+    'coding.jpg'
+  ]
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(){
+    this.updateImage();
+  }
+
+  updateImage(){
+    setInterval(() =>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(()=>{
+        this.showImage = true;
+      },10)
+    },8000)
+  }
 }
